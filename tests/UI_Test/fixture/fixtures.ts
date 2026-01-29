b26da3db4856071bcc094b74d88b5d695501801f
Sri-Vignesh-15 <srivigneshnatarajan@gmail.com>
2026-01-29 14:01:05 +0530

    Adding Prettier Dependencies to meet the coding standards

--- tests/UI_Test/fixture/fixtures.ts
+++ tests/UI_Test/fixture/fixtures.ts
@@ -1,17 +1,20 @@
 import { test as base } from 'playwright-bdd';
 import { LoginPage } from '../page/loginpage';
-import * as Pages from '../page/index'
+import * as Pages from '../page/index';
 import { Page } from '@playwright/test';
 
 type Myfixtures = {
-    logInPage: Pages.LoginPage;
-}
+  logInPage: Pages.LoginPage;
+};
 
-const createTestFunction = <T extends new (page:Page)  => InstanceType<T>>(PageClass: T) =>
-    ( {page}: {page: Page}, use: (fixture: InstanceType<T>) => Promise<void>) => 
-    use(new PageClass(page));   
+const createTestFunction =
+  <T extends new (page: Page) => InstanceType<T>>(PageClass: T) =>
+  (
+    { page }: { page: Page },
+    use: (fixture: InstanceType<T>) => Promise<void>
+  ) =>
+    use(new PageClass(page));
 
 export const test = base.extend<Myfixtures>({
-    logInPage: createTestFunction(Pages.LoginPage)
-})
-
+  logInPage: createTestFunction(Pages.LoginPage),
+});
--- tests/UI_Test/page/loginpage.ts
+++ tests/UI_Test/page/loginpage.ts
@@ -1,47 +1,47 @@
-import { Locator,Page,expect } from '@playwright/test'
+import { Locator, Page, expect } from '@playwright/test';
 import { BasePage } from './BasePage';
 
-export class LoginPage extends BasePage{
-    readonly website_Logo: Locator;
-    readonly my_Account_Btn: Locator;
-    readonly login_page: Locator;
-    readonly email_Field: Locator;
-    readonly password_Field: Locator;
-    readonly login_Btn: Locator;
-    readonly alert_Message: Locator;
+export class LoginPage extends BasePage {
+  readonly website_Logo: Locator;
+  readonly my_Account_Btn: Locator;
+  readonly login_page: Locator;
+  readonly email_Field: Locator;
+  readonly password_Field: Locator;
+  readonly login_Btn: Locator;
+  readonly alert_Message: Locator;
 
-    constructor(page: Page){
-        super(page);
-        this.website_Logo = page.getByRole('img', {name: 'Poco Electro'});
-        this.my_Account_Btn = page.getByRole('button', {name: ' My account'});
-        this.login_page = page.locator('#account-login');
-        this.email_Field = page.getByLabel('E-Mail Address');
-        this.password_Field = page.getByLabel('Password');
-        this.login_Btn = page.getByRole('button', {name:'Login'});
-        this.alert_Message = page.locator('.alert.alert-danger.alert-dismissible');
-    }
+  constructor(page: Page) {
+    super(page);
+    this.website_Logo = page.getByRole('img', { name: 'Poco Electro' });
+    this.my_Account_Btn = page.getByRole('button', { name: ' My account' });
+    this.login_page = page.locator('#account-login');
+    this.email_Field = page.getByLabel('E-Mail Address');
+    this.password_Field = page.getByLabel('Password');
+    this.login_Btn = page.getByRole('button', { name: 'Login' });
+    this.alert_Message = page.locator('.alert.alert-danger.alert-dismissible');
+  }
 
-    async navigateToURL(URL: string){
-        await this.page.goto(URL);
-        await expect(this.website_Logo).toBeVisible();
-    }
+  async navigateToURL(URL: string) {
+    await this.page.goto(URL);
+    await expect(this.website_Logo).toBeVisible();
+  }
 
-    async clickMyAccount(){
-        await this.my_Account_Btn.click();
-        await expect(this.login_page).toBeVisible();
-    }
+  async clickMyAccount() {
+    await this.my_Account_Btn.click();
+    await expect(this.login_page).toBeVisible();
+  }
 
-    async enterEmailAndPassword(email: string,password: string){
-        await this.email_Field.fill(email);
-        await this.password_Field.fill(password);
-    }
+  async enterEmailAndPassword(email: string, password: string) {
+    await this.email_Field.fill(email);
+    await this.password_Field.fill(password);
+  }
 
-    async clickLoginButton(){
-        await this.login_Btn.click();
-    }
+  async clickLoginButton() {
+    await this.login_Btn.click();
+  }
 
-    async verifyAlertMessage(){
-        await expect(this.alert_Message).toBeVisible();
-        // await expect(this.alert_Message).toHaveText(" Warning: No match for E-Mail Address and/or Password.");
-    }
+  async verifyAlertMessage() {
+    await expect(this.alert_Message).toBeVisible();
+    // await expect(this.alert_Message).toHaveText(" Warning: No match for E-Mail Address and/or Password.");
+  }
 }
